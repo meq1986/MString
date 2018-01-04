06b83d2d87866e1ec660b7e4de46052db73cf6f9
meq1986 <meq1986@163.com>
2018-01-04 09:48:06 +0800

    创建项目

---
+++ src/com/maeq/mstring/impl/MString.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.maeq.mstring.impl;
+
+/**
+ * @author maeq
+ *
+ */
+public class MString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/maeq/mstring/test/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.maeq.mstring.test;
+
+/**
+ * @author maeq
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String t = "Hello,world.";
+		System.out.println(t);
+		System.out.println(t + t);
+	}
+
+}
